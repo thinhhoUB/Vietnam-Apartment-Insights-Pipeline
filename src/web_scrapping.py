88cc24715b5ef98a7fea471cb8fe819fa2b8ac1f
Thinh Ho <hotaoxuanthinh2809@gmail.com>
2024-04-06 16:35:30 -0400

    added Chrome Driver function

---
+++ src/web_scrapping.py
@@ -0,0 +1,17 @@
+from seleniumbase import Driver
+from time import sleep
+from seleniumbase import Driver
+from bs4 import BeautifulSoup
+from datetime import datetime
+from sys import path
+import secrets
+import logging
+
+page_url= 'https://batdongsan.com.vn/cho-thue-can-ho-chung-cu-tp-hcm'
+
+def createChromeDriver(num_chrome):
+    chrome_drivers = []
+    for _ in range(num_chrome):
+        driver = Driver(uc_cdp=True, incognito=True,block_images=True,headless=True)
+        chrome_drivers.append(driver)
+    return chrome_drivers
