4fbb37094a1f5c1bf255959abc077f81aac6e42d
Thinh Ho <hotaoxuanthinh2809@gmail.com>
2024-04-10 14:26:41 -0400

    AWS lambda function added

---
+++ src/transformation.py
@@ -0,0 +1,95 @@
+import csv
+import json
+import boto3
+import time
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import io
+import re
+
+
+
+def lambda_handler(event, context):
+    
+    event_params=event["Records"][0]
+    
+    bucket=event_params["s3"]["bucket"]["name"]
+    key=event_params["s3"]["object"]["key"]
+    
+    
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket= bucket, Key= key)
+    csv_bytes = response['Body'].read()
+
+    # Convert bytes to DataFrame using pandas
+    apt_listing = pd.read_csv(io.BytesIO(csv_bytes))
+    
+    #helper functions
+    def remove_punctuation(my_str):
+        return None if my_str == None else re.sub(r'[^\w\s]', '', my_str.strip())
+        
+    def split_full_address(address):
+        parts = address.split(", ")
+        length = len(parts)
+        province = remove_punctuation(parts[-1]) if length >= 1 else None
+        district = remove_punctuation(parts[-2]) if length >= 2 else None
+        ward = remove_punctuation(parts[-3]) if length >= 3 else None
+        street = remove_punctuation(parts[-4]) if length >= 4 else None
+        return (street, ward, district, province) 
+        
+    def create_bed_bath(row):
+        bedroom = str(int(row['bedroom_number'])) if pd.notnull(row['bedroom_number']) else '1'
+        bathroom = str(int(row['bathroom_number'])) if pd.notnull(row['bathroom_number']) else '1'
+        return bedroom + ' - ' + bathroom
+  
+    print('change columns name')
+    columns_name = ["area (sq ft)","price ($)","frontage","alley_length_to_house","house_direction","balcony_direction","floor_number","bedroom_number","bathroom_number",
+    "legal_document","furnished","uploaded_date","expired_date","listing_article_tier","listing_id","full_address","latitude","longtitude","url"]
+    apt_listing.columns= columns_name
+    
+    print('drop unescessary columns')
+    apt_listing.drop(["frontage","alley_length_to_house","floor_number","legal_document","listing_article_tier"],inplace=True, axis=1)
+    
+    #print( 'Convert the date column to datetime format')
+    #apt_listing['uploaded_date'] = pd.to_datetime(apt_listing['uploaded_date'] , format='%d/%m/%Y')
+    #apt_listing['uploaded_date']  = apt_listing['uploaded_date'].dt.strftime('%m/%d/%Y')
+    #apt_listing['expired_date'] = pd.to_datetime(apt_listing['expired_date'] , format='%d/%m/%Y')
+    #apt_listing['expired_date']  = apt_listing['expired_date'].dt.strftime('%m/%d/%Y')
+    
+    print('square meter to square feet')
+    apt_listing["area (sq ft)"] = np.round(pd.to_numeric(apt_listing["area (sq ft)"].str.split().str[0]) * 10.76, 2)
+    
+    print('Vietnam Dong to USD')
+    round_price = lambda x: np.round(pd.to_numeric(x.replace(',', '').split()[0]) * 1000000 / 24000, 2) if 'triệu/tháng' in x else None
+    apt_listing['price ($)'] = apt_listing['price ($)'].apply(round_price)
+    
+    print('calculate price per sqft')
+    apt_listing['price_per_sqft'] = round(apt_listing['price ($)'] / apt_listing['area (sq ft)'], 2)
+    
+    print('Furnished column')
+    check_nan = lambda x: 'No' if x is None or x is np.nan else 'Yes'
+    apt_listing['furnished']= apt_listing['furnished'].apply(check_nan)
+    
+    print('extract address detail')
+    apt_listing['street'], apt_listing['ward'], apt_listing['district'], apt_listing['province'] = zip(*apt_listing['full_address'].apply(split_full_address))
+    
+    print('number of bedroom and bathroom')
+    number_of_room = lambda x: int(str(x).split()[0]) if 'phòng' in str(x) else None
+    apt_listing["bedroom_number"] = apt_listing["bedroom_number"].apply(number_of_room)
+    apt_listing["bathroom_number"] = apt_listing["bathroom_number"].apply(number_of_room)
+    apt_listing['bed_bath'] = apt_listing.apply(create_bed_bath, axis=1)
+    
+    print('change value of house direction and balcony direction')
+    translation_dict = {'Đông': 'East','Tây': 'West','Nam': 'South','Bắc': 'North','Đông - Nam': 'Southeast','Đông - Bắc': 'Northeast','Tây - Nam': 'Southwest','Tây - Bắc': 'Northwest',np.nan: None}
+    apt_listing['house_direction'] = apt_listing['house_direction'].map(translation_dict)
+    apt_listing['balcony_direction'] = apt_listing['balcony_direction'].map(translation_dict)
+    
+    new_bucket = 'processed-apt-listing-data'
+    new_key = 'processed/' + key  # Assuming you want to store processed files in 'processed/' prefix
+    csv_buffer = io.StringIO()
+    apt_listing.to_csv(csv_buffer, index=False)
+    s3_client.put_object(Bucket=new_bucket, Key=new_key, Body=csv_buffer.getvalue())
+    print("File successfully uploaded to new bucket:", new_bucket, "with key:", new_key)
+    
+    
--- src/web_scrapping.py
+++ src/web_scrapping.py
@@ -4,6 +4,7 @@ from seleniumbase import Driver
 from bs4 import BeautifulSoup
 from datetime import datetime
 from sys import path
+import pandas as pd
 import secrets
 import logging
 import re
@@ -42,4 +43,110 @@ def extract_coordinates(html_content):
     else:
         return [None,None]
     
+def process_single_property(property_url,chrome_driver):
+    # logging.info(property_url)
+    chrome_driver.get(property_url)
+    html_content = chrome_driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find property info
+    elements =  soup.find('div', class_='re__pr-specs-content js__other-info')
+    titles=elements.find_all('span', class_='re__pr-specs-content-item-title')
+    titles=[title.get_text() for title in titles]
+    values=elements.find_all('span', class_='re__pr-specs-content-item-value')
+    values=[value.get_text() for value in values]
+
+    # Find property address
+    address= soup.find('span', class_='re__pr-short-description js__pr-address').text
+    titles.append("Địa chỉ")
+    values.append(address)
+
+    # Find property map coordination
+    map_coor=soup.find('div', class_='re__section re__pr-map js__section js__li-other')
+    map_coor=extract_coordinates(str(map_coor))
+    titles.append("latitude")
+    titles.append("longtitude")
+    values.append(map_coor[0])
+    values.append(map_coor[1])
+
+    # Find date info
+    short_info=soup.find('div', class_='re__pr-short-info re__pr-config js__pr-config')
+    short_info_titles=short_info.find_all('span', class_='title')
+    short_info_titles=[title.get_text() for title in short_info_titles]
+    short_info_values=short_info.find_all('span', class_='value')
+    short_info_values=[value.get_text() for value in short_info_values]
+
+    # Merge 2 list
+    titles.extend(short_info_titles)
+    values.extend(short_info_values)
+
+
+    property_attribute=dict(zip(titles, values))
+    order_attribbute={}
+
+    all_attributes = [
+        "Diện tích","Mức giá",
+        "Mặt tiền","Đường vào",
+        "Hướng nhà","Hướng ban công",
+        "Số tầng","Số phòng ngủ",
+        "Số toilet","Pháp lý","Nội thất",
+        'Ngày đăng', 'Ngày hết hạn',
+        'Loại tin', 'Mã tin', 'Địa chỉ',
+        "latitude","longtitude"
+    ]
+    all_attributes = {key: None for key in all_attributes}
+
+    for attr in all_attributes:
+        if(attr not in property_attribute):
+            order_attribbute[attr]=None
+        else:
+            order_attribbute[attr]=property_attribute[attr]
+
+    order_attribbute['url']=property_url
+
+    return order_attribbute
+
+def process_single_page(page_url,chrome_driver,max_retry=1):
+    logging.info(f"The process's scrapping {page_url}...")
+    for attempt in range(max_retry + 1):
+        try:
+            chrome_driver.get(page_url)
+
+            # Check for Cloudflare bot detection
+            if "Cloudflare" in chrome_driver.page_source:
+                raise Exception("Cloudflare detected in the page source")
+
+            # Scrapping data for each property
+            html_content = chrome_driver.page_source
+            property_urls = extract_property_urls_single_page(page_url,html_content)
+            return property_urls
+        except Exception as e:
+            logging.info(f"Attempt {attempt + 1}: Error - {e}, try again in 5s")
+            sleep(5)
+
+    logging.info(f"All attempts failed. Returning None for {page_url}")
+    return None
+
+def process_multiple_pages(number_of_page,url,number_of_chrome_driver):
+
+  chrome_driver_lst= createChromeDriver(number_of_chrome_driver)
+  properties= []
+  for i in range(6,number_of_page+1):
+    #properties= []
+    page_url= url + '/p' + str(i)
+
+    print(f"Scraping data from {page_url}")
+
+    chrome_driver= secrets.choice(chrome_driver_lst)
+    single_page_listing= process_single_page(page_url,chrome_driver,max_retry=1)
+
+    for p in single_page_listing:
+      properties.append(process_single_property(p,chrome_driver))
+    apt_listing= pd.DataFrame(properties)
+    apt_listing.to_csv('apt_listing_page_' + str(i) + '.csv',index= False)
+  return properties
     
+
+
+if __name__ == "__main__":
+    print(process_multiple_pages(6,page_url,5))
